26244e4c094aa0431585b15e1d498051cd35570b
Michał Gołdys <michal.goldys@onet.pl>
2019-12-21 14:54:32 +0100

    Add application properties and project test folder

---
+++ src/test/java/pl/michalgoldys/InvoiceHelpSystem/InvoiceHelpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.michalgoldys.InvoiceHelpSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceHelpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
