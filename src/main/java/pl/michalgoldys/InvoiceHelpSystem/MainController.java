f14e0053430987fbbce2f17f1d85bd36d5860ee8
Michał Gołdys <michal.goldys@onet.pl>
2020-01-05 01:51:31 +0100

    Add method to expose objects from db to view
    Add findAll methods to get all objects to view

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/MainController.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/MainController.java
@@ -2,6 +2,7 @@ package pl.michalgoldys.InvoiceHelpSystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,10 @@ public class MainController {
     AccountingOrderService accountingOrderService;
 
     @GetMapping("/index")
-    private String mainPage(){
+    private String mainPage(Model model){
+
+        model.addAttribute("AccountingOrderList", accountingOrderService.findAll());
+
         return "index.html";
     }
 
