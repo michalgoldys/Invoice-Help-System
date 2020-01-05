c2f1903949c0cd78e7d5175bbc34920b919a088e
Michał Gołdys <michal.goldys@onet.pl>
2020-01-05 01:57:43 +0100

    Initial version of class thats populate database
    Implements CommanLineRunner and Lambda Expressions
    
    That adds basic entity

---
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/DatabaseLoader.java
@@ -0,0 +1,46 @@
+package pl.michalgoldys.InvoiceHelpSystem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class DatabaseLoader {
+
+    @Autowired
+    AccountingOrderService accountingOrderService;
+
+    @Bean
+    CommandLineRunner initDatabase(){
+        return args ->{
+
+            Order order = new Order(OrderType.INVOICE, 400.00d,"01.01.2019", "testowy opis obiektu");
+
+            Contractor contractor = new Contractor.Builder()
+                    .setContractorStreet("Warszawska")
+                    .setContractorCity("Warszawa")
+                    .setContractorPostalCode("01-234")
+                    .setContractorName("EXAMPLE")
+                    .setContractorTaxNumber("1231232121")
+                    .build();
+
+            ContractorContactDetails contractorContactDetails = new ContractorContactDetails.Builder()
+                    .setContactPersonName("M")
+                    .setContactPersonSurname("G")
+                    .setContactMobile("123456789")
+                    .setContactStreet("Ogrodnicza")
+                    .setContactCity("Warszawa")
+                    .setContactPostalCode("00-000")
+                    .setContactEmailAddress("exmaple@example.com")
+                    .build();
+
+            AccountingOrder accountingOrder = new AccountingOrder(order,contractor, contractorContactDetails);
+            log.info("Saving database entites:... " + accountingOrder);
+            accountingOrderService.save(accountingOrder);
+            log.info("Saved");
+        };
+    }
+}
