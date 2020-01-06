8378e356be928a4e552c406a45cc80c0593bbc84
Michał Gołdys <michal.goldys@onet.pl>
2020-01-06 13:42:18 +0100

    Implementation of method to show concrete object
    Using PathVariable annotion and service method to get right object
    And expose it to view

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/MainController.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,6 +21,15 @@ public class MainController {
         model.addAttribute("AccountingOrderList", accountingOrderService.findAll());
 
         return "index.html";
+    }
+
+    @GetMapping("/orderDetails/{id}")
+    private String orderDetails(@PathVariable long id,
+                                Model model){
+
+        model.addAttribute("AccountingOrderObject", accountingOrderService.findById(id));
+
+        return "orderDetails.html";
     }
 
     @GetMapping("/addNewOrder")
