0ca168ff3dc70ba9ee1edec225fbd5fe9ba84271
Michał Gołdys <michal.goldys@onet.pl>
2020-01-21 20:43:11 +0100

    Inital commit

---
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/OrderDetails.java
@@ -0,0 +1,43 @@
+package pl.michalgoldys.InvoiceHelpSystem;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name="order_details")
+public class OrderDetails {
+
+    @Column(name = "order_mode")
+    @Enumerated(value = EnumType.STRING)
+    @Getter
+    private OrderMode orderMode;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    Long orderModeId;
+
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "orderId")
+    private Order order;
+
+    @Getter
+    @Setter
+    @Column(name ="isClosed")
+    private Boolean isClosed;
+
+    @Getter
+    @Setter
+    @Column(name ="close_date")
+    private String closeDate;
+
+    protected OrderDetails(){
+    }
+
+    public OrderDetails(OrderMode orderMode) {
+        this.orderMode = orderMode;
+    }
+}
---
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/OrderMode.java
@@ -0,0 +1,4 @@
+package pl.michalgoldys.InvoiceHelpSystem;
+
+public enum OrderMode {
+}
