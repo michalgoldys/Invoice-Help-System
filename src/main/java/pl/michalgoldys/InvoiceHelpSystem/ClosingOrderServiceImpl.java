68d53402934f2f7762565d3f6a45816334db15b4
Michał Gołdys <michal.goldys@onet.pl>
2020-01-21 21:25:17 +0100

    Initial commit
    Implements interface with close method, and include private check-in methods

---
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/ClosingOrderServiceImpl.java
@@ -0,0 +1,55 @@
+package pl.michalgoldys.InvoiceHelpSystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Component
+public class ClosingOrderServiceImpl implements ClosingOrder {
+
+    @Autowired
+    AccountingOrderService accountingOrderService;
+
+    @Override
+    public void close(Long id) {
+
+        AccountingOrder accountingOrder = accountingOrderGetter(id);
+
+        if(isValidForClosing(accountingOrder)) {
+            accountingOrder.getOrder().getOrderDetails().setIsClosed(true);
+            accountingOrder.getOrder().getOrderDetails().setCloseDate(getCurrentTime());
+
+            accountingOrderService.save(accountingOrder);
+        }
+    }
+
+    private String getCurrentTime(){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        return localDateTime.format(formatter);
+    }
+
+    private Boolean isValidForClosing(AccountingOrder accountingOrder) {
+
+        if (Optional.ofNullable(accountingOrder.getOrder().getOrderDetails().getIsClosed()).isPresent()) {
+                if (accountingOrder.getOrder().getOrderDetails().getIsClosed()) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        else {
+                return true;
+        }
+    }
+
+    private AccountingOrder accountingOrderGetter(Long id){
+        return accountingOrderService.findById(id);
+    }
+}
