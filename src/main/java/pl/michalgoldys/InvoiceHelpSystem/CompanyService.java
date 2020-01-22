8eec78c417525d93ad6f5b62c0c2fb80d7867b10
Michał Gołdys <michal.goldys@onet.pl>
2020-01-22 20:44:52 +0100

    Initial of service to presist and retrive company entity
    Using interface/abstraction

---
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/CompanyService.java
@@ -0,0 +1,42 @@
+package pl.michalgoldys.InvoiceHelpSystem;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class CompanyService implements CompanyServiceInterface{
+
+    @Autowired
+    CompanySaveServiceImpl companySaveService;
+
+    @Autowired
+    CompanyGetByIdServiceImpl companyGetByIdService;
+
+    private Company company = Company.getInstance();
+
+    private static final Long ID = 1L;
+
+    @Override
+    public Company companyGetterService(){
+        log.info("Checking if there is company entity to get..");
+        if(Optional.ofNullable(companyGetByIdService.findById(ID)).isPresent()){
+            log.info("Returning company entity");
+            return  companyGetByIdService.findById(ID);
+        }
+        else {
+            log.info("Returning company instance");
+            return company;
+        }
+    }
+
+    @Override
+    public void companySaveService(Company company){
+        log.info("Saving company entity");
+        company.setCompanyId(ID);
+        companySaveService.save(company);
+    }
+}
