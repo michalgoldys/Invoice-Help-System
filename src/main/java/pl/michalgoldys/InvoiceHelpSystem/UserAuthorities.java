5814c80b56024624a3507c8ec30fe8057c0808c4
Michał Gołdys <michal.goldys@onet.pl>
2020-01-23 21:41:26 +0100

    Update fields and constructor

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/UserAuthorities.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/UserAuthorities.java
@@ -1,11 +1,5 @@
 package pl.michalgoldys.InvoiceHelpSystem;
 
 public enum UserAuthorities {
-    ROLE_ADMIN("ROLE_ADMIN"), ROLE_USER("ROLE_USER");
-
-    String role;
-
-    UserAuthorities(String role) {
-        this.role = role;
-    }
+    ROLE_ADMIN, ROLE_USER;
 }
