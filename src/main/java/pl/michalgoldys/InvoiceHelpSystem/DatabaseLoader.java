dc4ea5441ef8bffce052bcd69c9c3a86701676cd
Michał Gołdys <michal.goldys@onet.pl>
2020-01-27 18:53:52 +0100

    Add mechanism for creating default admin user

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/DatabaseLoader.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/DatabaseLoader.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @Slf4j
@@ -13,9 +17,16 @@ public class DatabaseLoader {
     @Autowired
     AccountingOrderService accountingOrderService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UserDAO userDAO;
     @Bean
     CommandLineRunner initDatabase(){
         return args ->{
+
+            log.info("Creating example objects..");
 
             Order order = new Order(OrderType.INVOICE, 400.00d,"01.01.2019", "testowy opis obiektu");
             OrderDetails orderDetails = new OrderDetails(OrderMode.CONTIOUS);
@@ -45,6 +56,26 @@ public class DatabaseLoader {
             log.info("Saving database entites:... " + accountingOrder);
             accountingOrderService.save(accountingOrder);
             log.info("Saved");
+
+
+            log.info("Creating default admin  user");
+
+            User user = new User("admin",passwordEncoder.encode("admin1234"),true);
+
+            UserRoles userRoles = new UserRoles(UserAuthorities.ROLE_ADMIN.toString());
+
+            log.info("ROLE: " + UserAuthorities.ROLE_ADMIN.toString());
+
+            List<UserRoles> listUserRoles = new ArrayList<UserRoles>();
+
+            listUserRoles.add(userRoles);
+
+            user.setUserRoles(listUserRoles);
+            userRoles.setUser(user);
+
+            userDAO.save(user);
+
+            listUserRoles.clear();
         };
     }
 }
