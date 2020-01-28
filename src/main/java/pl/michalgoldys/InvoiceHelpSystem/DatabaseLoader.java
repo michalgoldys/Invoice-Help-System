c491f808a5c000414c2858c700571a08d76aace9
Michał Gołdys <michal.goldys@onet.pl>
2020-01-28 20:27:21 +0100

    Update syntax with non memory database user and entity populator
    Add checker if default user and basic example entity exist

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/DatabaseLoader.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/DatabaseLoader.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Configuration
 @Slf4j
@@ -26,56 +27,62 @@ public class DatabaseLoader {
     CommandLineRunner initDatabase(){
         return args ->{
 
-            log.info("Creating example objects..");
+            log.info("Checking if there is any object to present.. ");
 
-            Order order = new Order(OrderType.INVOICE, 400.00d,"01.01.2019", "testowy opis obiektu");
-            OrderDetails orderDetails = new OrderDetails(OrderMode.CONTIOUS);
+            if(accountingOrderService.findAll().isEmpty())
+            {
+                log.info("Creating example objects..");
 
-            orderDetails.setOrder(order);
-            order.setOrderDetails(orderDetails);
+                Order order = new Order(OrderType.INVOICE, 400.00d,"01.01.2019", "testowy opis obiektu");
+                OrderDetails orderDetails = new OrderDetails(OrderMode.CONTIOUS);
 
-            Contractor contractor = new Contractor.Builder()
-                    .setContractorStreet("Warszawska")
-                    .setContractorCity("Warszawa")
-                    .setContractorPostalCode("01-234")
-                    .setContractorName("EXAMPLE")
-                    .setContractorTaxNumber("1231232121")
-                    .build();
+                orderDetails.setOrder(order);
+                order.setOrderDetails(orderDetails);
 
-            ContractorContactDetails contractorContactDetails = new ContractorContactDetails.Builder()
-                    .setContactPersonName("M")
-                    .setContactPersonSurname("G")
-                    .setContactMobile("123456789")
-                    .setContactStreet("Ogrodnicza")
-                    .setContactCity("Warszawa")
-                    .setContactPostalCode("00-000")
-                    .setContactEmailAddress("exmaple@example.com")
-                    .build();
+                Contractor contractor = new Contractor.Builder()
+                        .setContractorStreet("Warszawska")
+                        .setContractorCity("Warszawa")
+                        .setContractorPostalCode("01-234")
+                        .setContractorName("EXAMPLE")
+                        .setContractorTaxNumber("1231232121")
+                        .build();
 
-            AccountingOrder accountingOrder = new AccountingOrder(order,contractor, contractorContactDetails);
-            log.info("Saving database entites:... " + accountingOrder);
-            accountingOrderService.save(accountingOrder);
-            log.info("Saved");
+                ContractorContactDetails contractorContactDetails = new ContractorContactDetails.Builder()
+                        .setContactPersonName("M")
+                        .setContactPersonSurname("G")
+                        .setContactMobile("123456789")
+                        .setContactStreet("Ogrodnicza")
+                        .setContactCity("Warszawa")
+                        .setContactPostalCode("00-000")
+                        .setContactEmailAddress("exmaple@example.com")
+                        .build();
 
+                AccountingOrder accountingOrder = new AccountingOrder(order,contractor, contractorContactDetails);
+                log.info("Saving database entites:... " + accountingOrder);
+                accountingOrderService.save(accountingOrder);
+                log.info("Saved");
+            }
 
-            log.info("Creating default admin  user");
+            log.info("Checing if there is default user..");
 
-            User user = new User("admin",passwordEncoder.encode("admin1234"),true);
+            if(!Optional.ofNullable(userDAO.findByusername("admin")).isPresent())
+            {
+                log.info("Creating default admin  user");
 
-            UserRoles userRoles = new UserRoles(UserAuthorities.ROLE_ADMIN.toString());
+                User user = new User("admin",passwordEncoder.encode("admin1234"),true);
+                UserRoles userRoles = new UserRoles(UserAuthorities.ROLE_ADMIN.toString());
 
-            log.info("ROLE: " + UserAuthorities.ROLE_ADMIN.toString());
+                log.info("ROLE: " + UserAuthorities.ROLE_ADMIN.toString());
 
-            List<UserRoles> listUserRoles = new ArrayList<UserRoles>();
+                List<UserRoles> listUserRoles = new ArrayList<UserRoles>();
+                listUserRoles.add(userRoles);
 
-            listUserRoles.add(userRoles);
+                user.setUserRoles(listUserRoles);
+                userRoles.setUser(user);
 
-            user.setUserRoles(listUserRoles);
-            userRoles.setUser(user);
-
-            userDAO.save(user);
-
-            listUserRoles.clear();
+                userDAO.save(user);
+                listUserRoles.clear();
+            }
         };
     }
 }
