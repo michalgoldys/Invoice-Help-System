0f43de7a4273bc990812e4c6224708f91e705d6b
Michał Gołdys <michal.goldys@onet.pl>
2020-01-28 21:10:23 +0100

    Rename wrong spelled enum class

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/PaymentPeriod.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/PaymentPeriod.java
@@ -3,9 +3,9 @@ package pl.michalgoldys.InvoiceHelpSystem;
 public enum PaymentPeroid {
     SEVEN(7), FOURTEEN(14), THIRTY(30), SIXTY(60);
 
-    int paymentPeroid;
+    int paymentPeriod;
 
-    PaymentPeroid(int paymentPeroid) {
-        this.paymentPeroid = paymentPeroid;
+    PaymentPeroid(int paymentPeriod) {
+        this.paymentPeriod = paymentPeriod;
     }
 }
