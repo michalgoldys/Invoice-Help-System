b12696db90d1c1bc06e48952db087e7afe199869
Michał Gołdys <michal.goldys@onet.pl>
2020-01-28 21:11:17 +0100

    Fix wrong spellend variable and class name

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/PaymentPeriod.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/PaymentPeriod.java
@@ -1,11 +1,11 @@
 package pl.michalgoldys.InvoiceHelpSystem;
 
-public enum PaymentPeroid {
+public enum PaymentPeriod {
     SEVEN(7), FOURTEEN(14), THIRTY(30), SIXTY(60);
 
     int paymentPeriod;
 
-    PaymentPeroid(int paymentPeriod) {
+    PaymentPeriod(int paymentPeriod) {
         this.paymentPeriod = paymentPeriod;
     }
 }
