bc193ffd68fae93f9c4fc2d5b8aa1c17ba54ba06
Michał Gołdys <michal.goldys@onet.pl>
2020-02-24 21:00:53 +0100

    Polishing imports

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/CompanyService.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/CompanyService.java
@@ -2,7 +2,6 @@ package pl.michalgoldys.InvoiceHelpSystem;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
