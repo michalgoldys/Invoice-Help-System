fef0d6986376f2c8886263e69253140a54c9c4c7
Michał Gołdys <michal.goldys@onet.pl>
2020-02-24 21:24:38 +0100

    Add field, constructor and getter

--- src/main/java/pl/michalgoldys/InvoiceHelpSystem/OrderMode.java
+++ src/main/java/pl/michalgoldys/InvoiceHelpSystem/OrderMode.java
@@ -2,5 +2,16 @@ package pl.michalgoldys.InvoiceHelpSystem;
 
 public enum OrderMode {
 
-    SINGLE_TIME, CONTINUOUS;
+    SINGLE_TIME("Pojedyncze"), CONTINUOUS("Powtarzalne");
+
+    String name;
+
+    OrderMode(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
